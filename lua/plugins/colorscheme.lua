eb03c39d34ce92f31a881fd78c93e8e61020624f
Rafael Cacilhas <rafaelcacilhas@gmail.com>
2024-08-26 09:52:34 -0300

    lazy vim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ntk148v/habamax.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "habamax",
+    },
+  },
+}
